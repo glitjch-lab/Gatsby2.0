cb9c38323be34c460ea9f6382e6d805848fdc4f6
TJ Phan <t.jamesphan@gmail.com>
2022-05-06 10:45:33 -0700

    Modify files to confirm working links

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,16 +1,18 @@
 import React from 'react'
-// import { Link } from "gatsby";
-// import * as classes from "./navbar.module.css"
+import { Link } from "gatsby";
+import * as classes from "./navbar.module.css"
 
-export const Navbar = () => {
+const Navbar = () => {
   return (
     <nav className={classes.Navbar}>
-      {/* <h1>Navbar</h1>       */}
-      {/* <ul>
+      <h1>Navbar</h1>      
+      <ul>
         <Link className={classes.Link} to="/">Home</Link>
         <Link className={classes.Link} to="/about">About</Link>
         <Link className={classes.Link} to="/blogs">Blog</Link>
-      </ul> */}
+      </ul>
     </nav>
   )
 }
+
+export default Navbar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import * as React from "react"
-// import { Navbar } from "../components/Navbar.";
+import Navbar from "../components/navbar";
 
-const IndexPage = () => (
-  <div>
-    {/* <Navbar /> */}
+const IndexPage = () => {
+  return (
+
+    <div>
+    <Navbar />
     <h1>HELLO WORLD</h1>
   </div>
-)
+    )
+  }
 
 export default IndexPage;
