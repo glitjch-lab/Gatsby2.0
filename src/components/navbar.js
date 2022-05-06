4d7e58c19c88707ed2a968fad4f76e5cdc2c1397
TJ Phan <t.jamesphan@gmail.com>
2022-05-06 10:32:06 -0700

    Delete boilerplate files

---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import { Link } from "gatsby";
+// import * as classes from "./navbar.module.css"
+
+export const Navbar = () => {
+  return (
+    <nav className={classes.Navbar}>
+      {/* <h1>Navbar</h1>       */}
+      {/* <ul>
+        <Link className={classes.Link} to="/">Home</Link>
+        <Link className={classes.Link} to="/about">About</Link>
+        <Link className={classes.Link} to="/blogs">Blog</Link>
+      </ul> */}
+    </nav>
+  )
+}
---
+++ src/pages/blogs/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function BlogsPage() {
+  return (
+    <div>
+      <h1>
+      Blog's Page
+      </h1>
+    </div>
+  );
+}
